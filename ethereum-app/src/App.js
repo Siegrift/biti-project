1c2afb30a8df84de2bddae368728dbf047503c19
Emanuel Tesar <e.tesarr@gmail.com>
2021-04-21 14:37:53 +0200

    Very basic metamask + ethers app

---
+++ ethereum-app/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import {ethers} from 'ethers'
+import usePromise from 'react-use-promise'
+
+// eslint-disable-next-line
+const reloadPage = () => location.reload();
+
+function App() {
+  const [data, error] = usePromise(
+    async () => {
+      // Initialize metamask provider
+      await window.ethereum.enable()
+
+      // Reload the page in case metamask changes
+      window.ethereum.on('accountsChanged', reloadPage);
+      window.ethereum.on('chainChanged', reloadPage);
+      window.ethereum.on('disconnect', reloadPage);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      return {
+        account: await signer.getAddress(),
+        balance: await (await signer.getBalance()).toString(),
+        chain: await signer.getChainId(),
+      }
+    },
+    []
+  );
+
+  return (
+    <div className="App App-container">
+      <p style={{color: 'red'}}>{error && "Unexpected error"}</p>
+      <p>{!data && 'loading...'}</p>
+      {!!data && (
+        <>
+          <p>Account: {data.account}</p>
+          <p>Balance: {data.balance}</p>
+          <p>ChainId: {data.chain}</p>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
